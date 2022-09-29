dbb512add99dd6070f90d8b5099aceef1b059a8d
jenshr <jenshrage@gmail.com>
2022-09-29 21:13:01 +0200

    REPL to pull out during presentation

---
+++ src/REPLSCRIPT.svelte
@@ -0,0 +1,35 @@
+<script>
+	let count = 0;
+	let kill = 0;
+	
+	$: doubled = count * 2;
+	$: halved = count / 2;
+	$: doubleHalved = doubled - halved;
+	
+	function incrementCount() {
+		count++;
+	}
+	function killCount() {
+		count = 0;
+		kill++;
+	}
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<button on:click={killCount}>
+	Killed {kill} {kill === 1 ? 'time' : 'times'}
+</button>
+
+
+<div>
+	Doubled = {doubled}
+</div>
+<div>
+	Halved = {halved}
+</div>
+<div>
+	One and a half = {doubleHalved}
+</div>
